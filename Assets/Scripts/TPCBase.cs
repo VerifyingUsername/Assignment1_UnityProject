e4a5cc4ed87b152ce44970d5edea4d21b1743cc2
19-BenjaminLee <leekyben@gmail.com>
2023-11-28 01:49:22 +0800

    Fixed some animations and camera reposition
    Camera still clips walls

--- Assets/Scripts/TPCBase.cs
+++ Assets/Scripts/TPCBase.cs
@@ -9,6 +9,7 @@ namespace PGGE
     {
         protected Transform mCameraTransform;
         protected Transform mPlayerTransform;
+        protected float defaultDistance = 3.0f;
 
         public Transform CameraTransform
         {
@@ -29,13 +30,56 @@ namespace PGGE
         {
             mCameraTransform = cameraTransform;
             mPlayerTransform = playerTransform;
+
         }
 
         public void RepositionCamera()
         {
-            
-        }
+            //-------------------------------------------------------------------
+            // Implement here.
+            //-------------------------------------------------------------------
+            //-------------------------------------------------------------------
+            // Hints:
+            //-------------------------------------------------------------------
+            // check collision between camera and the player.
+            // find the nearest collision point to the player
+            // shift the camera position to the nearest intersected point
+            //-------------------------------------------------------------------
 
+            LayerMask wallMask = LayerMask.GetMask("Wall");
+            RaycastHit[] hits;
+            float maxDistance = 3f;
+
+            Vector3 mPlayerPos = mPlayerTransform.position + new Vector3(0, 1.5f, 0);
+            Vector3 newCamPos = mCameraTransform.position - mPlayerPos;
+           
+            hits = Physics.RaycastAll(mPlayerPos, newCamPos, maxDistance, wallMask);
+
+            
+            float nearestDistance = float.MaxValue;
+            RaycastHit nearestCollisionPoint;
+
+            
+
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.transform != mPlayerTransform && hit.collider.tag != "IgnoreCameraCollision")
+                {
+                    if (hit.distance < nearestDistance)
+                    {
+                        nearestDistance = hit.distance;
+                        nearestCollisionPoint = hit;
+
+
+                        if (nearestDistance < Mathf.Infinity)
+                        {
+                            
+                            mCameraTransform.position = nearestCollisionPoint.point;
+                        }
+                    }
+                }
+            }      
+        }
         public abstract void Update();
     }
 }
